d5aa2b8253ccf30dc512a2eb94b58bab5cf7df65
Pete King <peterjking@gmail.com>
2021-02-26 17:11:17 +0000

    Changes for part 8

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
-$IMAGE_NAME_AND_TAG="samples-weatherforecast:v7"
+$IMAGE_NAME_AND_TAG="samples-weatherforecast:v8"
 
 Write-Output "App [build]"
 docker build -t $IMAGE_NAME_AND_TAG .
--- unit-test.ps1
+++ unit-test.ps1
@@ -1,4 +1,4 @@
-$IMAGE_NAME_AND_TAG="samples-weatherforecast-unit-test:v7"
+$IMAGE_NAME_AND_TAG="samples-weatherforecast-unit-test:v8"
 
 Write-Output "Unit tests [build]"
 docker build --target unit-test -t $IMAGE_NAME_AND_TAG .
